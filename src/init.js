5fc19feb72c27538cfbcf959209c837df0c685b9
gissehel <gissehel@localhost>
2010-01-20 09:08:35 +0100

    Suppression des logs

--- src/init.js
+++ src/init.js
@@ -4,17 +4,17 @@
     undefined;
     function doinit () {
         if ((window.QILVGallery_overlays == undefined) || (window.VK == undefined)) { 
-            console.log("here");
+            // console.log("here");
             setTimeout(doinit, 100);
         } else {
-            console.log("thislog");
-            console.log([window.QILVGallery_overlays,window.VK]);
+            // console.log("thislog");
+            // console.log([window.QILVGallery_overlays,window.VK]);
             window.QILVGallery_overlays.init();
             window.jQuery.noConflict(true);
         }
     }
-    console.log("now");
+    // console.log("now");
     doinit();
-    console.log("there");
+    // console.log("there");
     
 })();

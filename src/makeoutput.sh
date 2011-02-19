bee49768b0c076e18a336ca351baf1676ccafbdf
Gissehel <Gissehel@localhost>
2011-02-19 13:27:27 +0100

    new loading process : only one js that contains all

--- src/makeoutput.sh
+++ src/makeoutput.sh
@@ -1,7 +1,14 @@
-wget -O ../output/jquery.min.js http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js
-coffee -o ../output -c *.coffee
-(cat ../output/jquery.min.js; yui-compressor ../output/VK.js; yui-compressor ../output/qilvgallery.js; yui-compressor ../output/qilvgallery-launch.js) > ../output/qilvgallery-total.min.js
-(cat ../output/jquery.min.js; yui-compressor ../output/VK.js; yui-compressor ../output/qilvgallery.js; yui-compressor ../output/gallery.js) > ../output/qilvgallery-configuration.min.js
-cp gallery.html ../output/gallery.html
+WORKING_DIR=../work
+OUTPUT_DIR=../output
+
+[ -e "$WORKING_DIR" ] || mkdir -p "$WORKING_DIR"
+[ -e "$OUTPUT_DIR" ] || mkdir -p "$OUTPUT_DIR"
+
+wget -O "$WORKING_DIR"/jquery.min.js http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js
+coffee -o "$WORKING_DIR" -c *.coffee
+(cat "$WORKING_DIR"/jquery.min.js; yui-compressor "$WORKING_DIR"/VK.js; yui-compressor "$WORKING_DIR"/qilvgallery.js; yui-compressor "$WORKING_DIR"/qilvgallery-launch.js) > "$OUTPUT_DIR"/qilvgallery-total.min.js
+(cat "$WORKING_DIR"/jquery.min.js; yui-compressor "$WORKING_DIR"/VK.js; yui-compressor "$WORKING_DIR"/qilvgallery.js; yui-compressor "$WORKING_DIR"/gallery.js) > "$OUTPUT_DIR"/qilvgallery-configuration.min.js
+cp gallery.html "$OUTPUT_DIR"/gallery.html
+
 
 

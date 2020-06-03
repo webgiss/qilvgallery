56adf0f9b9a40a9dccdec663b59b92d5dff0db71
gissehel <public-git-dev@gissehel.org>
2020-06-03 20:15:17 +0200

    Move code from boot.js to external deps

--- src/boot.js
+++ src/boot.js
@@ -1,43 +1,5 @@
-import DomAccess from './DomAccess';
-import ImageOverlayFactory from './ImageOverlayFactory';
-import GalleryOverlaysUi from './GalleryOverlaysUi';
-import VK from './VK';
-import GalleryOverlays from './GalleryOverlays';
+import { readyPromise, QILVGalleryInit } from './tools';
 
 (() => {
-    const readyPromise = new Promise((resolve, reject) => {
-        if (document.readyState === 'complete' || (document.readyState !== 'loading' && !document.documentElement.doScroll)) {
-            setTimeout(() => resolve(), 1);
-        } else {
-            const onContentLoaded = () => {
-                resolve();
-                document.removeEventListener('DOMContentLoaded', onContentLoaded, false);
-            }
-            document.addEventListener('DOMContentLoaded', onContentLoaded, false);
-        }
-    })
-
-    /**
-     * Initialise QILVGallery_init
-     */
-    const QILVGalleryInit = () => {
-        if (!window.QILVGalleryOverlays) {
-            /** @type {Object.<string, string>} */
-            const config = window.QILV_config || window.GM_values || {};
-            const domAccess = new DomAccess();
-            const imageOverlayFactory = new ImageOverlayFactory({ domAccess });
-            const galleryOverlaysUi = new GalleryOverlaysUi({ domAccess });
-            const vk = new VK({ config });
-            const galleryOverlays = new GalleryOverlays({
-                imageOverlayFactory,
-                galleryOverlaysUi,
-                vk,
-                config,
-            });
-            window.QILVGalleryOverlays = galleryOverlays;
-            galleryOverlays.init();
-        }
-    }
-
     readyPromise.then(() => QILVGalleryInit());
 })()
